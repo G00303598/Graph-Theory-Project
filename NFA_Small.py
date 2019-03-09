7f682c1881de98d567586acfebacac40cdb2647f
Morgan <G00303598@gmit.ie>
2019-03-09 00:16:12 +0000

    Bracket issue fix. Wasn't hadnling harder strings well. Working now on NFA construction

--- NFA_Small.py
+++ NFA_Small.py
@@ -1,3 +1,6 @@
 # This program is intended to construct a series of small NFA's
 # for use in a seperate program to construct larger NFA's
 
+# References:
+# https://swtch.com/~rsc/regexp/regexp1.html
+
--- ParserInfixPostfix.py
+++ ParserInfixPostfix.py
@@ -14,7 +14,7 @@ def convert_infix_to_postfix(infix_expression):
     print("INFIX: ", infix_expression)
     # loop through input
     for i, token in enumerate(infix_expression):
-        print(i, " >   Input: ", token, "   Stack: ", stack,"   Output: ", output)
+        # print(i, " >   Input: ", token, "   Stack: ", stack, "   Output: ", output)
 
         if token is '(':
             stack.append(token)
@@ -35,43 +35,57 @@ def convert_infix_to_postfix(infix_expression):
                 # while stack has a operator of greater precedence than input
                 while stack and token_precedence <= stack_precedence:
                     to_output = stack.pop()  # stores token to send to output
-                    output.append(to_output)
-
+                    if to_output is not '(':
+                        output.append(to_output)
                 # push lesser operator to stack
                 stack.append(token)
 
         # if right bracket encountered
-        if token == ')':
-            while stack[-1] != '(':
+        #if token is ')':
+        #    while stack[-1] is not '(':
+        #        print(stack[-1])
+        #        to_output = stack.pop()
+        #        output.append(to_output)
+        #    discard_token = stack.pop()  # ( found, pop from stack
+
+        # if right bracket encountered
+        if token is ')':
+            for i, el in enumerate(stack[-1]):
                 to_output = stack.pop()
-                output.append(to_output)
-            stack.pop()  # ( found, pop from stack
+                if el is not '(':
+                    output.append(to_output)
 
     # clear remainder on stack
     while stack:
         to_output = stack.pop()
-        output.append(to_output)
+        if to_output is not '(':
+            output.append(to_output)
 
     # print(output)  # prints as list
     print("POSTFIX: ", ''.join(output))  # prints as string
+
+    return output
 # END
 
 
 def check_precedence(input_token):
     if input_token is '^':
-        return 3
+        return 4
     if input_token is '*':
+        return 3
+    if input_token is '+' or input_token is '-' or input_token is '|':
         return 2
-    if input_token is '+' or input_token is '-':
+    if input_token is '.':
         return 1
     if input_token is ')' or input_token is '(':
-        return -1
+        return -2
 
 
 # Tests from: http://www.oxfordmathcenter.com/drupal7/node/628
-# convert_infix_to_postfix("A*B+C")  # Test 1
-# convert_infix_to_postfix("A+B*C")  # Test 2
-# convert_infix_to_postfix("A*(B+C)")  # Test 3
-# convert_infix_to_postfix("A-B+C")  # Test 4
-# convert_infix_to_postfix("A*B^C+D")  # Test 5
-convert_infix_to_postfix("A*(B+C*D)+E")  # Test 6
+# convert_infix_to_postfix("A*B+C")  # Test 1 -- A B * C +
+# convert_infix_to_postfix("A+B*C")  # Test 2 -- A B C * +
+# convert_infix_to_postfix("A*(B+C)")  # Test 3 -- A B C + *
+# convert_infix_to_postfix("A-B+C")  # Test 4 -- A B - C +
+# convert_infix_to_postfix("A*B^C+D")  # Test 5 -- A B C ^ * D +
+# convert_infix_to_postfix("A*(B+C*D)+E")  # Test 6 -- A B C D * + * E +
+convert_infix_to_postfix("(0|(1(01*(00)*0)*1)*)*")
